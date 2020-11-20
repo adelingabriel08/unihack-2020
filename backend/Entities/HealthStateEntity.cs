697166ea71442415787822ba0863ef9f8c396e4c
Munteanu <cristian.munteanu@ligaac.ro>
2020-11-20 23:08:36 +0200

    Add healthStateController

--- backend/Entities/HealthStateEntity.cs
+++ backend/Entities/HealthStateEntity.cs
@@ -1,7 +1,18 @@
 namespace unihack.Entities
 {
-    public class HealthStateEntity
+    public class HealthStateEntity : Entity
     {
+        public float Temperature { get; set; }
+        public bool Fever { get { return this.Temperature>37.2 ; } }
+        public bool DryCough { get; set; }
+        public bool RunnyNose { get; set; }
+        public bool TiredNess { get; set; }
+        public bool DifficultyInBreathing { get; set; }
+        public bool SoreThroat {get; set; }
+        public bool None_Symptom {get;}
+        public bool Pains { get; set; }
+        public bool NasalCongestion { get; set; }
+        public bool Diarrhea { get; set; }
         
     }
 }
