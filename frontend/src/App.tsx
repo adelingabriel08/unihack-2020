c5d9f2c5c47cd51bb884af21080774a8afbb2a49
Adelin Chis <Adelin.Chis@theaccessgroup.com>
2020-11-20 21:10:22 +0200

    dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 //1. import
 //include `react-big-scheduler/lib/css/style.css` for styles, link it in html or import it here
-import React from "react";
+import Button from "@material-ui/core/Button/Button";
+import React, { useState } from "react";
 
 export const App = () => {
+  const [button, setButton] = useState("Apasa-ma!");
   return (
-    <div>
-     asd
+    <div className="container my-5 d-flex justify-content-center">
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setButton("Tzeapa!")}
+      >
+        {button}
+      </Button>
     </div>
   );
 };
