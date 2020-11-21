cd3b4debc6c44f8a70754a92adae415b1f5894bd
Adelin <adelin.chis@ligaac.ro>
2020-11-21 19:49:58 +0200

    responsive app

--- frontend/src/Components/FilledBackground.tsx
+++ frontend/src/Components/FilledBackground.tsx
@@ -1,68 +1,17 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import './ComponentsCSS/Typography.css';
 
-export const FilledBackground = () => {
-  const background: React.CSSProperties = {
-    backgroundColor: "#f5f186",
-    backgroundImage: "linear-gradient(315deg, #f5f186 0%, #9dfbc8 74%)",
-  };
-  return (
-    <div
-      className="container-fluid d-flex align-items-center p-5"
-      style={background}
-    >
-      <div data-aos="fade-up" data-aos-duration="1000" className="container ">
-        <div className="row">
-          <Typography
-            variant="h2"
-            component="h3"
-            gutterBottom
-            style={{ fontWeight: "bold" }}
-            className="my-5"
-          >
-            What our doctors recommend
-          </Typography>
-          <div
-            className=" col-6"
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            data-aos-delay="500"
-          >
-            <img src="assets/images/stay_safe.svg" width="100%" alt="" />
-          </div>
-          <div
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            className="col-6 d-flex flex-column justify-content-around"
-          >
-            <Typography variant="h5" component="h5" gutterBottom>
-              People age 2 and older should wear masks in public settings and
-              when around people who don’t live in their household.
-            </Typography>
-            <Typography variant="h5" component="h5" gutterBottom>
-              Masks offer some protection to you and are also meant to protect
-              those around you, in case you are unknowingly infected with the
-              virus that causes COVID-19.
-            </Typography>
-            <Typography variant="h5" component="h5" gutterBottom>
-              A mask is NOT a substitute for social distancing. Masks should
-              still be worn in addition to staying at least 6 feet apart.
-            </Typography>
-            {/*<Typography variant="h5" component="h5" gutterBottom>*/}
-            {/*    Wash your hands with soap and water for at least 20 seconds or use hand sanitizer with at least*/}
-            {/*    60%*/}
-            {/*    alcohol after touching or removing your mask.</Typography>*/}
-            {/*<Typography variant="h5" component="h5" gutterBottom>*/}
-            {/*    Masks may not be necessary when you are outside by yourself away from others, or with other*/}
-            {/*    people*/}
-            {/*    who live in your household. However, some localities may have mask mandates while out in public*/}
-            {/*    and*/}
-            {/*    these mandates should always be followed.</Typography>*/}
-            {/*<Typography variant="h5" component="h5" gutterBottom>*/}
-            {/*    CDC is still studying the effectiveness of different types of masks and will update our*/}
-            {/*    recommendations as new scientific evidence becomes available.*/}
-            {/*</Typography>*/}
-          </div>
+
+export const FilledBackground = (props: any) => {
+    const background: React.CSSProperties = {
+        backgroundColor: "#f5f186",
+        backgroundImage: "linear-gradient(315deg, #f5f186 0%, #9dfbc8 74%)"
+    }
+    return <div className="container-fluid d-flex align-items-center p-5" style={background}>
+        <div data-aos="fade-up" data-aos-duration="1000" className="container ">
+            <div className="row">
+                {props.children}
+            </div>
         </div>
       </div>
     </div>
--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,60 +1,64 @@
-import { Button } from "@material-ui/core";
+import {BottomNavigation, BottomNavigationAction, Button} from "@material-ui/core";
 import React from "react";
 import "./ComponentsCSS/Navbar.css";
-import history from "./History";
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
 
 export const Navbar = () => {
-  return (
-    <nav className="container-fluid d-flex justify-content-between align-items-center">
-      <img src="assets/images/stay_safe.svg" height="100px" />
-      <div className="d-flex">
-        <div className="nav-item">
-          {/* icon here */}
-          <Button
-            onClick={() => history.push("/")}
-            variant="contained"
-            color="inherit"
-            style={{ color: "#1DB954" }}
-          >
-            Home
-          </Button>
-        </div>
-        <div className="nav-item">
-          {/* icon here */}
-          <Button variant="contained" color="inherit">
-            Covid Statistics
-          </Button>
-        </div>
-        <div className="nav-item">
-          {/* icon here */}
-          <Button variant="contained" color="inherit">
-            Protect Yourself
-          </Button>
-        </div>
-        <div className="nav-item">
-          {/* icon here */}
-          <Button variant="contained" color="inherit">
-            Check your Sympthoms
-          </Button>
-        </div>
-        <div className="nav-item">
-          {/* icon here */}
-          <Button
-            onClick={() => history.push("/login")}
-            variant="contained"
-            color="inherit"
-          >
-            Login
-          </Button>
-          <Button
-            onClick={() => history.push("/register")}
-            variant="contained"
-            color="inherit"
-          >
-            Register
-          </Button>
-        </div>
-      </div>
-    </nav>
-  );
+  const [value, setValue] = React.useState(0);
+    return (<>
+            <nav className="container-fluid d-flex justify-content-between align-items-center">
+                <img src="assets/images/stay_safe.svg" height="100px"/>
+                <div className="d-flex">
+                    <div className="nav-item d-lg-flex d-none">
+                        {/* icon here */}
+                        <Button variant="contained" color="inherit" style={{color: "#1DB954"}}>
+                            Home
+                        </Button>
+                    </div>
+                    <div className="nav-item d-lg-flex d-none">
+                        {/* icon here */}
+                        <Button variant="contained" color="inherit">
+                            Covid Statistics
+                        </Button>
+                    </div>
+                    <div className="nav-item d-lg-flex d-none">
+                        {/* icon here */}
+                        <Button variant="contained" color="inherit">
+                            Protect Yourself
+                        </Button>
+                    </div>
+                    <div className="nav-item d-lg-flex d-none">
+                        {/* icon here */}
+                        <Button variant="contained" color="inherit">
+                            Check your Sympthoms
+                        </Button>
+                    </div>
+                    <div className="nav-item">
+                        {/* icon here */}
+                        <Button variant="contained" color="inherit">
+                            Login
+                        </Button>
+                    </div>
+                </div>
+            </nav>
+            <nav className="d-lg-none">
+                <BottomNavigation
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                    showLabels
+                    className="bottom-nav"
+                >
+                    <BottomNavigationAction label="Recents" icon={<RestoreIcon/>}/>
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon/>}/>
+                    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon/>}/>
+                </BottomNavigation>
+                );
+            </nav>
+        </>
+    );
 };
