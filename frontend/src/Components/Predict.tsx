c85027a81c46f0b001ca0da2a4f10e102a7210e6
Adelin <adelin.chis@ligaac.ro>
2020-11-22 11:56:22 +0200

    predict states

--- frontend/src/Components/Predict.tsx
+++ frontend/src/Components/Predict.tsx
@@ -2,11 +2,29 @@ import {Slider, Typography} from "@material-ui/core";
 import React from "react";
 
 export const PredictComponent = (props: {prediction: number}) => {
-    const sliderValue = props.prediction*25;
+    const sliderValue = props.prediction*33;
+    console.log(props.prediction);
     return <div style={{margin: "200px 0px 400px 0px"}}>
         <Typography variant="h3" gutterBottom>
-            Your prediction
+            Your prediction in severity
         </Typography>
-        <Slider disabled defaultValue={sliderValue} aria-labelledby="disabled-slider"/>
+        <div className="px-5 mt-5">
+            <Slider disabled defaultValue={sliderValue} aria-labelledby="disabled-slider"/>
+        </div>
+        <div className="d-flex justify-content-between">
+            <Typography variant="h5" gutterBottom>
+                Very Low
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+                Low
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+                Medium
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+                High
+            </Typography>
+        </div>
+
     </div>
 }
